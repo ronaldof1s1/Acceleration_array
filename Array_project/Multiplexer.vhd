18bef68310a6b132b5b6dc486957699371ee8fa8
Ronaldo de Figueiredo Silveira <ronaldof1s1@gmail.com>
2017-09-19 15:56:37 -0300

    add multiplexer to project

--- Array_project/Multiplexer.vhd
+++ Array_project/Multiplexer.vhd
@@ -1,1 +1,23 @@
+library ieee;
+use ieee.std_logic_1164.ALL;
 
+entity Multiplexer is
+  generic (N : integer);
+	port(
+		A,B	: in std_logic_vector (N-1 downto 0);
+		sel	: in std_logic;
+		result	: out std_logic_vector (N-1 downto 0)
+	);
+end Multiplexer;
+
+architecture mux of Multiplexer is
+begin
+  process(sel,A,B)
+  begin
+    if(sel = '0') then
+      result <= A;
+    else
+      result <= B;
+    end if;
+  end process;
+end mux;
