90c8274f2651b2b4f7b5d83da6be4f51487620f7
Ronaldo de Figueiredo Silveira <ronaldof1s1@gmail.com>
2017-09-19 15:59:22 -0300

    add a multiplier to project

--- Array_project/Multiplier.vhd
+++ Array_project/Multiplier.vhd
@@ -1,1 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.ALL;
+use ieee.std_logic_arith.ALL;
+use ieee.std_logic_unsigned.ALL;
 
+entity Multiplier is
+  Generic (N : integer);
+  port(
+    A, B : in  std_logic_vector(N/2 downto 0);
+    result: out std_logic_vector(N-1 downto 0)
+  );
+end Multiplier;
+
+architecture Multiplier of Multiplier is
+  begin
+    process(A,B)
+    begin
+      result <= A * B;
+    end process;
+end Multiplier;
