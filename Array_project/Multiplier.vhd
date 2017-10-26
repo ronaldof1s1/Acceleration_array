d8ddcb7b62c579f857bc10503f5862afa1ed18a3
Ronaldo de Figueiredo Silveira <ronaldof1s1@gmail.com>
2017-10-26 12:23:48 -0200

    bugfixes on main file and multiplier. Bitstream have 94 bits now

--- Array_project/Multiplier.vhd
+++ Array_project/Multiplier.vhd
@@ -5,15 +5,17 @@ use ieee.std_logic_unsigned.ALL;
 
 entity Multiplier is
   port(
-    A, B : in  std_logic_vector(3 downto 0);
+    A, B : in  std_logic_vector(7 downto 0);
     result: out std_logic_vector(7 downto 0)
   );
 end Multiplier;
 
 architecture Multiplier of Multiplier is
+  signal res : std_logic_vector(15 downto 0);
   begin
-    process(A,B)
+    res <= A * B;
+    process(A,B,res)
     begin
-      result <= A * B;
+      result <= res(15 downto 8);
     end process;
 end Multiplier;
