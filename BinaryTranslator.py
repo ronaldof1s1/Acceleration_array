1acc2b6a0dc4a0d35f57ba427aaf42742dd82e46
Ronaldo Silveira <ronaldof1s1@gmail.com>
2018-05-22 22:37:18 -0300

    Updated Binary translator

--- BinaryTranslator.py
+++ BinaryTranslator.py
@@ -7,27 +7,7 @@ def read_file(path):
 class Binary_translator:
     level = Array_level(3, 2, 1, 1)
 
-    def get_operation_code(self, op_string):
-            op_string = op_string.upper()
-            
-            if op_string == 'ADD':
-                return '000'
-            
-            elif op_string == 'SUB':
-                return '001'
-            
-            elif op_string == 'AND':
-                return '010'
-            
-            elif op_string == 'OR':
-                return '011'
-            
-            elif op_string == 'XOR':
-                return '100'
-            
-            else:
-                raise Exception
-
+    
     def prepare_line(self, line):
         words = line.split()
         operation = words[0].upper()
@@ -53,3 +33,57 @@ class Binary_translator:
                 continue
             else:
                 break
+
+    def get_mux_selector(self, string):
+        if string == 'r0':
+            return '00'
+        elif string == 'r1':
+            return '01'
+        elif string == 'r2':
+            return '10'
+        else:
+            return '11'
+
+    def get_operation_code(self, op_string):
+            op_string = op_string.upper()
+            
+            if op_string == 'ADD':
+                return '000'
+            
+            elif op_string == 'SUB':
+                return '001'
+            
+            elif op_string == 'AND':
+                return '010'
+            
+            elif op_string == 'OR':
+                return '011'
+            
+            elif op_string == 'XOR':
+                return '100'
+            
+            else:
+                raise Exception
+
+    def translate_levels(self):
+        bitstream = ''
+       
+        for read_alu in self.level.read_ALUs:
+            for (in1, in2) in read_alu[0]:
+                bitstream += self.get_mux_selector(in1) + self.get_mux_selector(in2) 
+       
+        for write_alus in self.level.write_ALUs:
+            for out in write_alus:
+                bitstream += self.get_mux_selector(out)
+       
+        for op_line in self.level.ALUs_OPs:
+            for op in op_line:
+                bitstream += self.get_operation_code(op)
+        
+        for (in1, in2) in self.level.read_mult:
+            bitstream += self.get_mux_selector(in1) + self.get_mux_selector(in2)
+
+        for out in self.level.write_mult:
+            bitstream +=  self.get_mux_selector(out)
+        
+        
