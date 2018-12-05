1bc2e9bb3753b8da73d57f86fef42df90b29e5a7
Ronaldo Silveira <ronaldof1s1@gmail.com>
2018-12-05 14:04:37 -0300

    change register to be a 32 bit register

--- Array_project/Generic_Register.vhd
+++ Array_project/Generic_Register.vhd
@@ -3,23 +3,23 @@ use ieee.std_logic_1164.ALL;
 
 entity Generic_Register is
   port(
-		input	: in std_logic_vector (7 downto 0);
+		input	: in std_logic_vector (31 downto 0);
 		clk	: in std_logic;
 		store	: in std_logic;
 		clr	: in std_logic;
-		output	: out std_logic_vector (7 downto 0)
+		output	: out std_logic_vector (31 downto 0)
 	);
 end Generic_Register;
 
 architecture reg of Generic_Register is
-	signal in_buffer	: std_logic_vector(7 downto 0);
+	signal in_buffer	: std_logic_vector(31 downto 0);
 begin
 	in_buffer <= input;
 
 	process(clk, clr)
 	begin
     if clr = '1' then
-      output <= "00000000";
+      output <= "00000000000000000000000000000000";
     elsif rising_edge(clk) and clr = '0' then
 		  if store = '1' then
 			  output <= in_buffer;
