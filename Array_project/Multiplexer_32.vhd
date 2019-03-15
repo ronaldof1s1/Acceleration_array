dc1afff10f5bebb9c7a31f229650d7330103fc9f
Ronaldo Silveira <ronaldof1s1@gmail.com>
2019-03-15 00:09:16 -0300

    add signals to sensitivity list of multiplexer_32

--- Array_project/Multiplexer_32.vhd
+++ Array_project/Multiplexer_32.vhd
@@ -12,9 +12,12 @@ entity Multiplexer_32 is
 	);
 end Multiplexer_32;
 
-architecture mux of Multiplexer_32 is
+architecture mux_32 of Multiplexer_32 is
 begin
-  process(sel)
+  process(sel,in0, in1, in2, in3, in4, in5, in6, in7, 
+  in8, in9, in10, in11, in12, in13, in14, in15, 
+  in16, in17, in18, in19, in20, in21, in22, in23, 
+  in24, in25, in26, in27, in28, in29, in30, in31)
   begin
     if (sel = "00000") then
       result <= in0;
@@ -82,4 +85,4 @@ begin
       result <= in31;
     end if;
   end process;
-end mux;
+end mux_32;
